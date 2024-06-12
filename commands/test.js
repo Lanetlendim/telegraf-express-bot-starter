54f5c5b444078f8d9594b2f857e7a954a52c239b
Dear Samuel <lupinay1@gmail.com>
2024-06-12 17:49:44 +0300

    helooo

---
+++ commands/test.js
@@ -0,0 +1,15 @@
+export default {
+    name: 'test',
+    async execute(ctx) {
+        const args = ctx.message.text.split(' ').slice(1).join(' ');
+
+        if (!args) return ctx.reply('Give an argument!');
+
+        try {
+            ctx.reply(args);
+        } catch (error) {
+            console.error('Error:', error);
+            return ctx.reply(`Error: ${error.message}`);
+        }
+    }
+};
